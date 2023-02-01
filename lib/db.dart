f72940dfff291742b3e31d3d60f06bb21d0164bc
chairbender <kwhipke1@gmail.com>
2023-01-31 20:57:26 -0800

    change how DB model is defined to use .drift files temporarily use in memory db only

--- lib/db.dart
+++ lib/db.dart
@@ -18,4 +18,32 @@ class MyDb extends _$MyDb {
 
   @override
   int get schemaVersion => 1;
+
+  @override
+  MigrationStrategy get migration {
+    return MigrationStrategy(
+      onCreate: (Migrator m) async {
+        await m.createAll();
+        await createGame(0, "Spy x Family Tamagotchi");
+        await createGame(1, "Digital Monster X Version XC");
+        await createShell(0, 0, "Anyatchi Pink");
+        await createShell(1, 0, "SPY Green");
+        await createShell(2, 1, "Red (Japanese)");
+        await createShell(3, 1, "Translucent Red and Gold (English)");
+        await createShell(4, 1, "Metallic Navy and Silver (English)");
+      },
+      // onUpgrade: (Migrator m, int from, int to) async {
+      //   if (from < 2) {
+      //     // we added the dueDate property in the change from version 1 to
+      //     // version 2
+      //     await m.addColumn(todos, todos.dueDate);
+      //   }
+      //   if (from < 3) {
+      //     // we added the priority property in the change from version 1 or 2
+      //     // to version 3
+      //     await m.addColumn(todos, todos.priority);
+      //   }
+      // },
+    );
+  }
 }
